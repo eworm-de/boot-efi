b1b6c2a5198fad941f2b9871a902ef960ca96931
Kay Sievers <kay@vrfy.org>
2015-11-19 14:04:39 +0100

    test: support /vmlinuz /initrd.img

--- test/test-efi-create-disk.sh
+++ test/test-efi-create-disk.sh
@@ -1,4 +1,6 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -e
 
 # create GPT table with EFI System Partition
 rm -f test-efi-disk.img
@@ -19,12 +21,21 @@ cp bootx64.efi mnt/EFI/Boot/bootx64.efi
 mkdir mnt/EFI/bus1
 echo -n "bus1-0815" | iconv -f UTF-8 -t UTF-16LE > mnt/release.txt
 echo -n "foo=yes bar=no" | iconv -f UTF-8 -t UTF-16LE > mnt/options.txt
+
+linux=/boot/$(cat /etc/machine-id)/$(uname -r)/linux
+test -e "$linux" || linux=/vmlinuz
+test -e "$linux" || exit 1
+
+initrd=/boot/$(cat /etc/machine-id)/$(uname -r)/initrd
+test -e "$initrd" || initrd=/initrd.img
+test -e "$initrd" || exit 1
+
 objcopy \
   --add-section .release=mnt/release.txt --change-section-vma .release=0x20000 \
   --add-section .options=mnt/options.txt --change-section-vma .options=0x30000 \
   --add-section .splash=test/bus1.bmp --change-section-vma .splash=0x40000 \
-  --add-section .linux=/boot/$(cat /etc/machine-id)/$(uname -r)/linux --change-section-vma .linux=0x2000000 \
-  --add-section .initrd=/boot/$(cat /etc/machine-id)/$(uname -r)/initrd --change-section-vma .initrd=0x3000000 \
+  --add-section .linux=$linux --change-section-vma .linux=0x2000000 \
+  --add-section .initrd=$initrd --change-section-vma .initrd=0x3000000 \
   stubx64.efi mnt/EFI/bus1/bus1.efi
 
 sync
