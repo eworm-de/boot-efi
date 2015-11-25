208ebd1e17d27dd8a07d021721d7619bdc9c78cf
Kay Sievers <kay@vrfy.org>
2015-11-25 12:53:20 +0100

    test-efi-create-disk: use /tmp

--- test/test-efi-create-disk.sh
+++ test/test-efi-create-disk.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+ROOT=$(mktemp -d /tmp/efi-tmpXXX)
+
 # create GPT table with EFI System Partition
 rm -f test-efi-disk.img
 dd if=/dev/null of=test-efi-disk.img bs=1M seek=512 count=1
@@ -10,15 +12,15 @@ parted --script test-efi-disk.img "mklabel gpt" "mkpart ESP fat32 1MiB 511MiB" "
 # create FAT32 file system
 LOOP=$(losetup --show -f -P test-efi-disk.img)
 mkfs.vfat -F32 ${LOOP}p1
-mkdir -p mnt
-mount ${LOOP}p1 mnt
+mkdir -p $ROOT
+mount ${LOOP}p1 $ROOT
 
-mkdir -p mnt/EFI/Boot
-cp bootx64.efi mnt/EFI/Boot/bootx64.efi
+mkdir -p $ROOT/EFI/Boot
+cp bootx64.efi $ROOT/EFI/Boot/bootx64.efi
 
-mkdir mnt/EFI/bus1
-echo -n "bus1-0815" | iconv -f UTF-8 -t UTF-16LE > mnt/release.txt
-echo -n "foo=yes bar=no" | iconv -f UTF-8 -t UTF-16LE > mnt/options.txt
+mkdir $ROOT/EFI/bus1
+echo -n "bus1-0815" | iconv -f UTF-8 -t UTF-16LE > $ROOT/release.txt
+echo -n "foo=yes quiet" | iconv -f UTF-8 -t UTF-16LE > $ROOT/options.txt
 
 linux=linux
 test -e "$linux" || linux=/boot/$(cat /etc/machine-id)/$(uname -r)/linux
@@ -31,14 +33,14 @@ test -e "$initrd" || initrd=/initrd.img
 test -e "$initrd" || exit 1
 
 objcopy \
-  --add-section .release=mnt/release.txt --change-section-vma .release=0x20000 \
-  --add-section .options=mnt/options.txt --change-section-vma .options=0x30000 \
+  --add-section .release=$ROOT/release.txt --change-section-vma .release=0x20000 \
+  --add-section .options=$ROOT/options.txt --change-section-vma .options=0x30000 \
   --add-section .splash=test/bus1.bmp --change-section-vma .splash=0x40000 \
   --add-section .linux=$linux --change-section-vma .linux=0x2000000 \
   --add-section .initrd=$initrd --change-section-vma .initrd=0x3000000 \
-  stubx64.efi mnt/EFI/bus1/bus1.efi
+  stubx64.efi $ROOT/EFI/bus1/bus1.efi
 
 sync
-umount mnt
-rmdir mnt
+umount $ROOT
+rmdir $ROOT
 losetup -d $LOOP
